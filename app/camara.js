1da150af3dbe347fbf5e35c8ae9c420f91cdff52
alejandro <alejandro.soto@usach.cl>
2014-10-05 22:09:00 -0400

    chat uno a uno funcional. Bug en cámara

--- app/camara.js
+++ app/camara.js
@@ -4,21 +4,65 @@ URL = window.URL || window.mozURL || window.webkitURL;
 
 var App = {};
 
-App.camara = function (){
-	var video = window.streamLocal;
+var idRemoto;
 
-	navigator.getUserMedia({video: true, audio: true}, function (stream){
-		video.src = URL.createObjectURL(stream);
-		App.socket.emit('video', video);
+//var urlVideoLocal;
+var video = window.streamLocal;
+var videoRemoto = window.streamRemoto;
+
+App.camara = function (){
+	var urlVideoLocal;
+	navigator.getUserMedia({video: true, audio: true}, function (stream, urlVideoLocal){
+		urlVideoLocal = URL.createObjectURL(stream);
+		video.src = urlVideoLocal;
+		//App.socket.emit('video', video);
 	}, function (error){
 		console.log('Error: ' + error);
 	});
+
+	return urlVideoLocal;
 };
 
 App.socket = io.connect('/');
 
 App.socket.on('ready', function (){
-	msge.textContent = "WebSockets estan listos";
+	msje.textContent = "WebSockets estan listos";
+});
+
+App.socket.on('no_users', function (){
+	msje.textContent = "No hay mas usuarios conectados, espere a que ingrese alguien más...";
+});
+
+App.socket.on('send_id', function (user, video_src){
+	console.log('soy '+ document.getElementById("username").value + ' y llega el id '+user+' con video '+video_src);
+	App.socket.emit('establece_conexion', user, video_src);
+	//videoRemoto.src = video_src;
+	//App.socket.to(user_id).emit('video', video);
+	//App.socket.join(''+user_id);
+	//App.socket.in(user_id).emit('video', video);
+	//App.socket.emit('add_room', user);
+	//idRemoto = user;
+	//io.sockets.socket(user).emit('establece_conexion', )
+});
+
+App.socket.on('conexion_ready', function (user, video){
+	console.log('soy '+ document.getElementById("username").value + ' y llega el id '+user.id+' con video '+video);
+});
+
+App.socket.on('video', function (video){
+	videoRemoto = video;
+});
+
+App.socket.on('connection_ready', function (user){
+	userRemoto.textContent = user.username;
+	userLocal.textContent = document.getElementById("username").value;
+});
+
+App.socket.on('recibe_msje', function (msje, username){
+	console.log('Mensaje recibido: '+msje);
+	var etiquetaMsje = document.createElement('p');
+	etiquetaMsje.textContent = username + ' dice: ' + msje;
+	document.querySelector('#conversacion').appendChild(etiquetaMsje);
 });
 
 function registraUsuario(){
@@ -32,10 +76,24 @@ function registraUsuario(){
 			console.log("Se aprieta el boton");
 			document.querySelector("#inicioSesion").className = "hide";
 			document.querySelector("#chat").className = "col-md-12";
-			App.socket.emit('new_user', username);
-			App.camara();
+			//console.log('Enviando src video: '+urlVideoLocal);
+			App.socket.emit('new_user', username, App.camara());
 		}
 	}
 }
 
 registraUsuario();
+
+document.querySelector("#enviarMensaje").onclick = function (){
+	var username = document.getElementById("username").value;
+	var msjeTxt = document.querySelector('#escritura').value;
+
+	if(msjeTxt != ""){
+		App.socket.emit('envia_msje', msjeTxt, username);
+		escritura.value = "";
+		var etiquetaMsje = document.createElement('p');
+		etiquetaMsje.textContent = username + ' dice: ' + msjeTxt;
+		document.querySelector('#conversacion').appendChild(etiquetaMsje);
+	}
+}
+
--- server/socket.js
+++ server/socket.js
@@ -6,25 +6,74 @@ module.exports = function (server){
 	var usersConectados = [];
 	var usersEnEspera = [];
 
-	function estableceConexion(){
-		if(usersEnEspera.length >= 2){
-			
-		}
-	}
+	//var io = require('socket.io').listen(server);
 
 	webSocket.on('connection', function (socket){
+	//io.sockets.on('connection', function (socket){
 		socket.emit('ready');
-		socket.on('video', function (video){
-			
-		});
 
-		socket.on('new_user', function(username){
+		/*
+		socket.on('video', function (video){
+				
+		});
+		*/
+
+		socket.on('new_user', function(username, video_src){
 			user = {
 				id: socket.id,
 				username: username
 			}
 
 			usersEnEspera.push(user);
+
+			if(usersEnEspera.length >= 2){
+				
+				for(i=0; i<usersEnEspera.length; i++){
+					console.log(i+".- "+usersEnEspera[i].username+' - id: '+usersEnEspera[i].id);
+				}
+				
+				var user1 = usersEnEspera.shift();
+				var user2 = usersEnEspera.shift();
+
+				//socket.join(user1.id)
+
+				usersConectados.push({user1: user1, user2: user2});
+
+				socket.to(user1.id).emit('send_id', user2.id, video_src);
+				//socket.to(user2.id).emit('send_id', user1.id, video_src);
+
+			}
+			else{
+				socket.emit('no_users');	
+			}
+		});
+
+		socket.on('establece_conexion', function (user, video){
+			console.log('Se establece la conexion.');
+			for (i=0; i<usersConectados.length; i++){
+				if(usersConectados[i].user2.id == user){
+					socket.to(user).emit('conexion_ready', usersConectados[i].user1, video);
+				}
+			}
+		});
+
+		socket.on('envia_msje', function (msje, username){
+			console.log('Largo de la lista: '+usersConectados.length)
+			for (i=0; i<usersConectados.length; i++){
+				if(usersConectados[i].user1.username == username){
+					socket.to(usersConectados[i].user2.id).emit('recibe_msje', msje, username);
+					console.log('user1 - mensaje enviado: '+msje+' al usuario '+usersConectados[i].user2.username+' desde el usuario: '+usersConectados[i].user2.username);
+				}
+				else if(usersConectados[i].user2.username == username){
+					socket.to(usersConectados[i].user1.id).emit('recibe_msje', msje, username);
+					console.log('user2 - mensaje enviado: '+msje+' al usuario '+usersConectados[i].user1.username+' desde el usuario: '+usersConectados[i].user2.username);
+				}
+			}
+		});
+
+		socket.on('add_room', function(user){
+			socket.join(user.id);
+			socket.to(user.id).emit('connection_ready', user);
 		});
 
 	});
